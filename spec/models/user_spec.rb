ce06fcde29d7ad5cba3794973e7829d11a121e7c
Kensei Sakai <kensei.sakai@gmail.com>
2020-03-23 09:30:24 +0900

    Implement username validation
    Sure to have username
    Add this validation spec to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe User, type: :model do
   it 'has a valid factory' do
     expect(build(:user)).to be_valid
   end
+
+  describe 'validation' do
+    context 'without username' do
+      it 'is not valid' do
+        expect(build(:user, username: '')).not_to be_valid
+      end
+    end
+  end
 end
