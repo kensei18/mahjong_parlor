ad89b226f9f4878ab66a462ea1c4925fee8be1aa
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-05 12:03:38 +0900

    Add create to parlors controller

--- app/controllers/parlors_controller.rb
+++ app/controllers/parlors_controller.rb
@@ -7,5 +7,19 @@ class ParlorsController < ApplicationController
   end
 
   def create
+    @parlor = Parlor.new(parlor_params)
+    if @parlor.save
+      flash[:success] = "新しい店舗を登録しました！"
+      redirect_to root_url
+    else
+      flash[:danger] = "すでに登録済みの店舗です"
+      render 'new'
+    end
+  end
+
+  private
+
+  def parlor_params
+    params.require(:parlor).permit(:name, :address, :latitude, :longitude)
   end
 end
