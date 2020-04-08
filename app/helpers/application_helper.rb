e74197fb4c9aba98986052f8482c2605077c0e1d
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-08 23:06:50 +0900

    Add flash message to application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,19 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when "success"
+      "success"
+    when "error"
+      "danger"
+    when "alert"
+      "warning"
+    when "notice"
+      "info"
+    else
+      flash_type.to_s
+    end
+  end
 end
