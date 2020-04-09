10cf93f5efd2853dc8d6f22eedb2775373850426
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-09 20:30:11 +0900

    Setup action mailer
    Use gmail account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'https'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.gmail[:user_name],
+    password:             Rails.application.credentials.gmail[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
