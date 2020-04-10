f94416a6a32282156867258afae0f673c24a5f7e
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-10 16:15:13 +0900

    Add specs for bootstrap_class_for to application_helper_spec.rb

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
     context "with some page_title" do
@@ -27,6 +17,38 @@ RSpec.describe ApplicationHelper, type: :helper do
     context "when page_title is nil" do
       it "returns only the base title" do
         expect(full_title(nil)).to eq "Mahjong Parlor"
+      end
+    end
+  end
+
+  describe "#bootstrap_class_for" do
+    context "with 'success'" do
+      it "returns 'success'" do
+        expect(bootstrap_class_for('success')).to eq 'success'
+      end
+    end
+
+    context "with 'error'" do
+      it "returns 'danger'" do
+        expect(bootstrap_class_for('error')).to eq 'danger'
+      end
+    end
+
+    context "with 'alert'" do
+      it "returns 'warning'" do
+        expect(bootstrap_class_for('alert')).to eq 'warning'
+      end
+    end
+
+    context "with 'notice'" do
+      it "returns 'info'" do
+        expect(bootstrap_class_for('notice')).to eq 'info'
+      end
+    end
+
+    context "with 'info'" do
+      it "returns 'info'" do
+        expect(bootstrap_class_for('info')).to eq 'info'
       end
     end
   end
