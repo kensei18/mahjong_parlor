d6f3feca5dfb4a22703a1b0320888eec59f511d9
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-11 20:15:40 +0900

    Create admin user

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,3 +1,15 @@
-User.create!(username: "テストユーザー",
-             email: "test@example.com",
-             password: "password")
+User.create!(
+  [
+    {
+      username: Rails.application.credentials.admin[:username],
+      email: Rails.application.credentials.admin[:email],
+      password: Rails.application.credentials.admin[:password],
+      admin: true,
+    },
+    {
+      username: "テストユーザー",
+      email: "test@example.com",
+      password: "password",
+    }
+  ]
+)
