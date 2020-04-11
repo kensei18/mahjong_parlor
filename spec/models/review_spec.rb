f9d9016e8cdab4c196162c4b52dbbeb01c752172
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-11 17:29:23 +0900

    Add validations to Review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,66 @@ RSpec.describe Review, type: :model do
   it 'has a valid factory' do
     expect(build(:review)).to be_valid
   end
+
+  describe "title validation" do
+    context "without title" do
+      it 'is invalid' do
+        expect(build(:review, title: "")).not_to be_valid
+      end
+    end
+
+    context "with 30 Japanese characters title" do
+      it 'is valid' do
+        valid_title = "あ" * 30
+        expect(build(:review, title: valid_title)).to be_valid
+      end
+    end
+
+    context "with more than 30 Japanese characters title" do
+      it 'is invalid' do
+        invalid_title = "あ" * 31
+        expect(build(:review, title: invalid_title)).not_to be_valid
+      end
+    end
+  end
+
+  describe "content validation" do
+    context "without content" do
+      it 'is invalid' do
+        expect(build(:review, content: "")).not_to be_valid
+      end
+    end
+  end
+
+  describe "smoking validation" do
+    context "without smoking review" do
+      it 'is invalid' do
+        expect(build(:review, smoking: nil)).not_to be_valid
+      end
+    end
+  end
+
+  describe "cleanliness validation" do
+    context "without cleanliness review" do
+      it 'is invalid' do
+        expect(build(:review, cleanliness: nil)).not_to be_valid
+      end
+    end
+  end
+
+  describe "service validation" do
+    context "without service review" do
+      it 'is invalid' do
+        expect(build(:review, service: nil)).not_to be_valid
+      end
+    end
+  end
+
+  describe "customer validation" do
+    context "without customer review" do
+      it 'is invalid' do
+        expect(build(:review, customer: nil)).not_to be_valid
+      end
+    end
+  end
 end
