cdebd65c2333a0a697fe93a19e798c9a42805657
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-11 21:29:24 +0900

    Add id to test user

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,12 +1,14 @@
 User.create!(
   [
     {
+      id: 1,
       username: Rails.application.credentials.admin[:username],
       email: Rails.application.credentials.admin[:email],
       password: Rails.application.credentials.admin[:password],
       admin: true,
     },
     {
+      id: 2,
       username: "テストユーザー",
       email: "test@example.com",
       password: "password",
