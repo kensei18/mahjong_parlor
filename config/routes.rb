6cbfc41fa2ba27f7920dc71e2cd8f6eabaa423fe
Kensei Sakai <kensei.sakai@gmail.com>
2020-04-12 13:53:49 +0900

    Add reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'parlors#index'
 
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
+                     controllers: { registrations: 'users/registrations' }
+
   resources(:users, only: [:index, :show]) do
     member do
       get :delete
     end
   end
 
-  resources :parlors, only: [:index, :show, :new, :create]
-  resources :reviews, only: [:new]
+  resources(:parlors) do
+    resources :reviews, shallow: true
+  end
 end
